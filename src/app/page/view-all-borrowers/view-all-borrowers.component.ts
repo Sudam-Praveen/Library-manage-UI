7410b98ca11693f0ce468b91d7915920b7593480
Sudam-Praveen <sudam.peramuna@gmail.com>
2024-02-27 02:10:37 +0530

    addBorrower has changed

--- src/app/page/view-all-borrowers/view-all-borrowers.component.ts
+++ src/app/page/view-all-borrowers/view-all-borrowers.component.ts
@@ -14,6 +14,7 @@ export class ViewAllBorrowersComponent implements OnInit {
   public allBorrowersList: any;
   public response: any;
   public selectedBorrower: any
+  public existsBorrower: any;
 
   constructor(httpClient: HttpClient) {
     this.http = httpClient;
@@ -22,10 +23,10 @@ export class ViewAllBorrowersComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadBorrowers();
-    
-      AOS.init({
-        duration: 500
-      });
+
+    AOS.init({
+      duration: 500
+    });
 
   }
 
@@ -39,7 +40,7 @@ export class ViewAllBorrowersComponent implements OnInit {
   public borrower = {
     firstName: null,
     lastName: null,
-    userName:null,
+    userName: null,
     email: null,
     address: null,
     doB: null,
@@ -50,7 +51,7 @@ export class ViewAllBorrowersComponent implements OnInit {
     this.borrower = {
       firstName: null,
       lastName: null,
-      userName:null,
+      userName: null,
       email: null,
       address: null,
       doB: null,
@@ -64,8 +65,7 @@ export class ViewAllBorrowersComponent implements OnInit {
     }
   }
 
-  addBorrower() {
-
+  isExistsBorrower() {
     if (this.borrower.contactNumber && this.borrower.contactNumber.length !== 10) {
       Swal.fire({
         title: "Invalid Phone Number!",
@@ -75,16 +75,23 @@ export class ViewAllBorrowersComponent implements OnInit {
       console.log("Contact Number:", this.borrower.contactNumber);
 
     } else {
+      this.http.get(`http://localhost:8081/borrower/isExistBorrower/${this.borrower.userName}`).subscribe(data => {
+        console.log(data)
+        this.existsBorrower = data;
+        this.addBorrower(this.existsBorrower);
+      })
+    }
+
+  }
+
+  addBorrower(existsBorrower: any) {
+
+
+    if (existsBorrower == false) {
 
       this.http.post("http://localhost:8081/borrower/add", this.borrower).subscribe(data => {
         console.log(data);
-        if (data == null) {
-          Swal.fire({
-            title: "Available Person!",
-            html: `This <b>${this.borrower.contactNumber}</b> Number is Available please <b>Update</b>!`,
-            icon: "error"
-          });
-        } else {
+        
           console.log(data);
           this.response = data;
           Swal.fire({
@@ -94,80 +101,87 @@ export class ViewAllBorrowersComponent implements OnInit {
           });
           this.clearFields();
           this.loadBorrowers();
-        }
-
-
       })
 
+    } else {
+      Swal.fire({
+        title: "Available Person!",
+        html: `This <b>${this.borrower.userName}</b> has been added`,
+        icon: "error"
+      });
+      this.clearFields();
     }
 
-  }
 
-  setSelectedBorrower(Borrower: any) {
-    this.selectedBorrower = Borrower;
-  }
-  removeBorrower() {
-    this.http.delete(`http://localhost:8081/borrower/delete/${this.selectedBorrower.id}`, { responseType: 'text' })
+
+ 
+}
+
+setSelectedBorrower(Borrower: any) {
+  this.selectedBorrower = Borrower;
+}
+removeBorrower() {
+  this.http.delete(`http://localhost:8081/borrower/delete/${this.selectedBorrower.id}`, { responseType: 'text' })
+    .subscribe(data => {
+      console.log(data);
+
+
+      Swal.fire({
+        title: "Deleted!",
+        html: `The Borrower <b style="color: red;">"${this.selectedBorrower.firstName}"</b>  is deleted`,
+        icon: "success"
+      });
+      this.selectedBorrower = null;
+      this.loadBorrowers();
+    }, error => {
+      console.error("Error deleting book:", error);
+    })
+
+}
+
+updateBorrower() {
+
+  if (this.selectedBorrower.contactNumber && this.selectedBorrower.contactNumber.length !== 10) {
+    Swal.fire({
+      title: "Invalid Phone Number!",
+      html: `please <b>Recheck</b> phone Number!`,
+      icon: "error"
+    });
+    console.log("Contact Number:", this.selectedBorrower.contactNumber);
+    this.loadBorrowers();
+  } else {
+
+    this.http.put("http://localhost:8081/borrower/update", this.selectedBorrower)
       .subscribe(data => {
         console.log(data);
-
-
         Swal.fire({
-          title: "Deleted!",
-          html: `The Borrower <b style="color: red;">"${this.selectedBorrower.firstName}"</b>  is deleted`,
+          title: "Updated!",
+          html: `The Borrower <b>${this.selectedBorrower.firstName}</b> is updated`,
           icon: "success"
         });
         this.selectedBorrower = null;
         this.loadBorrowers();
-      }, error => {
-        console.error("Error deleting book:", error);
       })
-
   }
-
-  updateBorrower() {
-
-    if (this.selectedBorrower.contactNumber && this.selectedBorrower.contactNumber.length !== 10) {
-      Swal.fire({
-        title: "Invalid Phone Number!",
-        html: `please <b>Recheck</b> phone Number!`,
-        icon: "error"
-      });
-      console.log("Contact Number:", this.selectedBorrower.contactNumber);
-      this.loadBorrowers();
-    } else {
-
-      this.http.put("http://localhost:8081/borrower/update", this.selectedBorrower)
-        .subscribe(data => {
-          console.log(data);
-          Swal.fire({
-            title: "Updated!",
-            html: `The Borrower <b>${this.selectedBorrower.firstName}</b> is updated`,
-            icon: "success"
-          });
-          this.selectedBorrower = null;
-          this.loadBorrowers();
-        })
-    }
-  }
+}
 
 
 
   public searchBorrowers(key: string) {
-    console.log(key);
-    const results: any[] = [];
-    for (const borrower of this.allBorrowersList) {
-      if (borrower.firstName.toLowerCase().indexOf(key.toLowerCase()) !== -1
-        || borrower.lastName.toLowerCase().indexOf(key.toLowerCase()) !== -1
-        || borrower.contactNumber.toLowerCase().indexOf(key.toLowerCase()) !== -1
-        || borrower.address.toLowerCase().indexOf(key.toLowerCase()) !== -1) {
-        results.push(borrower);
-      }
-    }
-    this.allBorrowersList = results;
-    if (results.length === 0 || !key) {
-      this.loadBorrowers();
+  console.log(key);
+  const results: any[] = [];
+  for (const borrower of this.allBorrowersList) {
+    if (borrower.firstName.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || borrower.lastName.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || borrower.contactNumber.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || borrower.address.toLowerCase().indexOf(key.toLowerCase()) !== -1) {
+      results.push(borrower);
     }
   }
+  this.allBorrowersList = results;
+  if (results.length === 0 || !key) {
+    this.loadBorrowers();
+  }
+}
 
 }
