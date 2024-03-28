b27066fc173f2b47bed0787e304955cddff9d92b
Sudam-Praveen <sudam.peramuna@gmail.com>
2024-03-29 03:16:55 +0530

    implemented returning process with validation and  giving fine value if exists and increacing book qty when returning

--- src/app/page/live-borrowing/live-borrowing.component.ts
+++ src/app/page/live-borrowing/live-borrowing.component.ts
@@ -11,30 +11,124 @@ import AOS from 'aos'
 export class LiveBorrowingComponent implements OnInit {
 
   private http;
-  public allBorrowedDetails:any;
-  public returnedDate:any="";
+  public allBorrowedDetails: any;
+  public returnedDate: any = "";
+  public selectedDetailObject: any;
+  public fineValue: any;
   constructor(httpClient: HttpClient) {
     this.http = httpClient;
   }
   ngOnInit(): void {
     this.loadBorrowedDetails();
   }
-  loadBorrowedDetails(){
-    this.http.get("http://localhost:8082/borrow/getAll-borrowed-details").subscribe(data=>{
+  loadBorrowedDetails() {
+    this.http.get("http://localhost:8082/borrow/getAll-borrowed-details").subscribe(data => {
       console.log(data);
-      this.allBorrowedDetails=data;
+      this.allBorrowedDetails = data;
     })
   }
 
-  returnBooks(borrowerDetail:any){
+  updateQty(book: any) {
+    book.qty++;
+    this.http.put("http://localhost:8080/book/update", book)
+      .subscribe(data => {
+        console.log("increasing qty");
+        
+        console.log(data);
+
+      })
+  }
+
+  increaseBookQty() {
+    const bookIDArray: any = this.selectedDetailObject.books;
+    console.log(bookIDArray);
+    bookIDArray.forEach((element: any) => {
+      
+      this.http.get(` http://localhost:8080/book/search/${element}`)
+        .subscribe(data => {
+          console.log(data);
+          this.updateQty(data)
+        });
+    });
+  }
+
+  returnBooks(borrowerDetail: any) {
+
+    this.selectedDetailObject = borrowerDetail;
     const dateFormatRegex = /^\d{4}-\d{2}-\d{2}$/;
 
-    
-    if(!dateFormatRegex.test(borrowerDetail.returnedDate)){
-      alert("date type should be yyyy-mm-dd")
-      borrowerDetail.returnedDate=""
-    }else{
-      alert("good")
+
+    if (!dateFormatRegex.test(borrowerDetail.returnedDate)) {
+      Swal.fire({
+        title: "Invalid Date!",
+        html: "Please enter the date in yyyy-mm-dd format.",
+        icon: "error"
+      });
+      borrowerDetail.returnedDate = ""
+    } else {
+      console.log(borrowerDetail);
+      this.http.post("http://localhost:8082/borrow/find-the-fine", this.selectedDetailObject).subscribe(data => {
+        console.log(data);
+        this.fineValue = data;
+
+        if (this.fineValue > 0) {
+          Swal.fire({
+            title: "Fine Applicable !",
+            html: `Have a Fine of <b style="color: red;">"RS.${this.fineValue}.00"</b> up to date! `,
+            showCancelButton: true,
+            confirmButtonText: "Pay & Return"
+
+          }).then((result) => {
+
+            if (result.isConfirmed) {
+              this.selectedDetailObject.status = "returned";
+              this.selectedDetailObject.fine = this.fineValue;
+              //increase the book qty in allbooks
+              this.increaseBookQty();
+              this.http.post("http://localhost:8082/borrow/add-borrow-details", this.selectedDetailObject).subscribe(data => {
+                console.log(data);
+
+              })
+
+              Swal.fire("Book(s) Returned !", "", "success");
+              this.loadBorrowedDetails();
+
+
+            } else if (result.isDenied) {
+              Swal.fire("Changes are not saved", "", "info");
+            }
+          });
+        } else {
+
+          Swal.fire({
+            title: "No Fine Applicable !",
+            html: `Book(s) Can be Returned :) `,
+            showCancelButton: true,
+            confirmButtonText: "Return Book(s)"
+
+          }).then((result) => {
+
+            if (result.isConfirmed) {
+              this.selectedDetailObject.status = "returned";
+              this.selectedDetailObject.fine = this.fineValue;
+              //increase the book qty in allbooks
+              this.increaseBookQty()
+              this.http.post("http://localhost:8082/borrow/add-borrow-details", this.selectedDetailObject).subscribe(data => {
+                console.log(data);
+
+              })
+
+              Swal.fire("Book(s) Returned !", "", "success");
+              this.loadBorrowedDetails();
+
+            } else if (result.isDenied) {
+              Swal.fire("Changes are not saved", "", "info");
+            }
+          });
+
+        }
+
+      })
     }
   }
 }
